6ddc9e8de550dea83ccdc285380e6623621c79ea
Dustin Walker <dustin.walker@email.wsu.edu>
2023-08-13 19:32:29 -0400

    Fixed namespace in error.cshtml

--- Pages/Error.cshtml.cs
+++ Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace blaze_vtt.Pages;
+namespace BlazeVtt.Pages;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
